b3451a58e421986b4f9bbc04602666ac4c7d01cc
master <pnayimbila@st.ug.edu.gh>
2023-04-24 13:26:26 +0000

    Added js code to make each of buttons produce its respective sound when clicked using if condition.

--- index.js
+++ index.js
@@ -1,17 +1,64 @@
 
 var buttonListLength = document.querySelectorAll(".drum").length;
 
-console.log(buttonListLength);
+//console.log(buttonListLength);
 
 for  (var x = 0; x < buttonListLength; x++) {
     document.querySelectorAll("button")[x].addEventListener("click", function(){
-       // alert("I have been clicked");
 
-        var audio = new Audio("sounds/tom-1.mp3");
+        //this.style.color = "white";
 
-        audio.play();
+        if (this.innerHTML === "w") {
+
+            var audio = new Audio("sounds/tom-1.mp3");
+
+            audio.play();
+        }
+
+        else if (this.innerHTML === "a") {
+             var audio = new Audio("sounds/tom-2.mp3");
+
+             audio.play();
+        }
+
+        else if (this.innerHTML === "s") {
+
+            var audio = new Audio("sounds/tom-3.mp3");
+
+            audio.play();
+        }
+
+        else if (this.innerHTML === "d") {
+
+            var audio = new Audio("sounds/tom-4.mp3");
+
+            audio.play();
+        }
+
+        else if (this.innerHTML === "j") {
+            var audio = new Audio("sounds/snare.mp3");
+
+            audio.play();
+        }
+
+        else if (this.innerHTML === "k"){
+            var audio = new Audio("sounds/crash.mp3");
+
+            audio.play();
+        }
+
+        else {
+            var audio = new Audio("sounds/kick-bass.mp3");
+
+            audio.play();
+        }
+       
     })
 }
+
+// var audio = new Audio("sounds/tom-1.mp3");
+
+// audio.play();
 
 
 

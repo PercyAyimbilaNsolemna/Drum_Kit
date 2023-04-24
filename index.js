908456ec0822fd413882b927b18502b7930c0236
master <pnayimbila@st.ug.edu.gh>
2023-04-24 13:41:24 +0000

    Added js code to make the buttons produce sound using the switch statement.

--- index.js
+++ index.js
@@ -8,6 +8,61 @@ for  (var x = 0; x < buttonListLength; x++) {
 
         //this.style.color = "white";
 
+        var buttonInnerHTML = this.innerHTML;
+
+        switch (buttonInnerHTML) {
+            case "w":
+                    var tom1 = new Audio("sounds/tom-1.mp3");
+
+                    tom1.play();
+                break;
+
+            case "a":
+                    var tom2 = new Audio("sounds/tom-2.mp3");
+
+                    tom2.play();
+                break;
+
+            case "s":
+                    var tom3 = new Audio("sounds/tom-3.mp3");
+
+                    tom3.play();
+                break;
+
+            case "d":
+                    var tom4 = new Audio("sounds/tom-4.mp3");
+
+                    tom4.play();
+                break;
+
+            case "j":
+                    var snare = new Audio("sounds/snare.mp3");
+
+                    snare.play();
+                break;
+
+            case "k":
+                    var crash = new Audio("sounds/crash.mp3");
+
+                    crash.play();
+                break;
+
+            case "l":
+                    var kick_bass = new Audio("sounds/kick-bass.mp3");
+
+                    kick_bass.play();
+                break;
+
+        
+            default:
+                console.log(buttonInnerHTML);
+                break;
+        }
+
+        /*
+
+        Used if condition to play the appropriate sounds
+
         if (this.innerHTML === "w") {
 
             var audio = new Audio("sounds/tom-1.mp3");
@@ -52,6 +107,8 @@ for  (var x = 0; x < buttonListLength; x++) {
 
             audio.play();
         }
+
+        */
        
     })
 }

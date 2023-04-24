676d7887ae815dd9ca9d8902a7ca50b0099c395f
master <pnayimbila@st.ug.edu.gh>
2023-04-24 12:14:35 +0000

    Added js code to produce a sound when any of the buttons is clicked.

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ console.log(buttonListLength);
 
 for  (var x = 0; x < buttonListLength; x++) {
     document.querySelectorAll("button")[x].addEventListener("click", function(){
-        alert("I have been clicked");
+       // alert("I have been clicked");
+
+        var audio = new Audio("sounds/tom-1.mp3");
+
+        audio.play();
     })
 }
 
